8675c49ef0544101841043a05d2147fd74c7d170
ironkamosi <alintonchambers@gmail.com>
2021-02-17 22:17:50 +0100

    modifed the test for the counterDisplay funct

--- script.js
+++ script.js
@@ -138,21 +138,20 @@ function counterDisplay(numberOfEpisodes) {
   // counter for number of episode found in search
   // let counterDisplay;
   let counterDisplay = document.querySelector("#counterDisplayID");
-  
+  let navBarContainer = document.querySelector("#navBarContainer");
+
   // if (removeCounter) {
   //   removeCounter.remove()
   // }
 
   if (counterDisplay=== undefined) {
-  //let counterDisplay = document.createElement("div");
+  let counterDisplay = document.createElement("div");
   counterDisplay.setAttribute("id", "counterDisplayID");
   counterDisplay.style.backgroundColor = "blue";
   counterDisplay.style.margin = "0.5em";
   navBarContainer.appendChild(counterDisplay);
-
   }
   
-  let navBarContainer = document.querySelector("#navBarContainer");
  
   // rootElem.appendChild(counterDisplay);
   //let counterDisplay = document.getElementById("counterDisplayID");
