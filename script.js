ac321613c4078b8f833b22ebea6c854fe5cd81b3
ironkamosi <alintonchambers@gmail.com>
2021-02-27 01:53:55 +0100

    Update syntax for 400 challenge

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
 //You can edit ALL of the code here
+// global variable 
+function compare(showOne, showTwo) {
+  // its comparing the content of the elements, it tries to understand the position of the string elements
+  // console.log(showOne);
 
-// removes data from the dom
-function removeEpisodes(episodeList) {
-  let episodeContainers = document.getElementsByClassName(
-    "episode-container-class-name"
-  );
-  episodeContainers.innerHTML = "";
-  episodeList.forEach((element) => {
-    // loops over the array and removes each episode from the dom when the eventListener is activated
-    episodeContainers.parentNode.removeChild(element);
-  });
+  if (showOne.name.toLowerCase() < showTwo.name.toLowerCase()) {
+    return -1;
+  }
+  if (showOne.name.toLowerCase() < showTwo.name.toLowerCase()) {
+    return 1;
+  }
+  return 0;
 }
+
 // when logic is done call in the eventListener
 // tv input selector
 function addShowSelector(showData) {
   const rootElem = document.getElementById("root");
 
-  let select = document.getElementById("select-tv"); // this only adds the show select if it is not present in the DOM
-  console.log("test", select);
+ // let select = document.getElementById("select-tv"); // this only adds the show select if it is not present in the DOM
+  // console.log("test", select);
 
-  if (select === null) {
-    // this only works if the element does not exist
+//  if (select === null) {
+    // this only works if the element does not exist 
     select = document.createElement("select");
     select.setAttribute("id", "select-tv");
     rootElem.appendChild(select);
 
-    showData.forEach((show) => {
+    showData.sort(compare).forEach((show) => {
+      //  when the function processes the forEach the array will be sorted
       let option = document.createElement("option");
       option.innerHTML = `${show.name}`; // loops over the tv show data
       select.appendChild(option);
     });
-  }
-
+ // }
+// loose a track of the episode list because errors occur when it tries to 
+  //compare the two fields and the array 
+  // it also fails with the hide and show
   select.addEventListener("change", function (event) {
-    let episodeContainers = document.getElementsByClassName(
-      "episode-container-class-name"
-    ); // An array of episode container
-    let episodeIndex = 0;
-    let episodeCounter = 0;
+    let episodeContainers = document.getElementsByClassName("episode-container-class-name"); // An array of episode container
+    // let episodeIndex = 0;
+    // let episodeCounter = 0;
     let optionId = event.target.options[event.target.selectedIndex].index;
     let showId = showData[optionId].id;
     // for every item duplicate we need an test to see if it exists
 
     getAllEpisodesApi(showId); // this call gets the all the episodes
+    //addDropDownMenu(episodeList) // creates an error
+    // console.log("test", showData[optionId]);
 
-   // console.log("test", showData[optionId]);
+
     let styleContainer = document.getElementById("styleContainer");
-    styleContainer.innerHTML = "";
+    styleContainer.innerHTML = ""; // its removing all the episode containers
 
     // fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
     //   .then((response) => response.json().then((data) => data))
@@ -82,13 +87,19 @@ function addShowSelector(showData) {
 
 function addFooter() {
   const rootElem = document.getElementById("root");
-  let footer = document.createElement("footer");
-  footer.innerHTML = "<p>https://tvmaze.com/</p>";
-  rootElem.appendChild(footer);
+  let footer = document.getElementById("footer");
+  if (footer === null) {
+    footer = document.createElement("footer");
+    footer.setAttribute("id", "footer");
+    footer.innerHTML = "<p>https://tvmaze.com/</p>";
+    rootElem.appendChild(footer);
+  }
 }
 
 const hideEpisode = (episode) => {
   episode.style.display = "none";
+  // console.log("test",episode);
+
 };
 
 const showEpisode = (episode) => {
@@ -115,23 +126,37 @@ function addSingleEpisode(episode) {
   episodeContainer.style.margin = "1.5em";
 
   let title = document.createElement("h2");
-  title.style.textAlign = "center";
-  title.innerHTML = `${episode.name}: S${formatSeasonNum(
-    episode.season
-  )}E${formatSeasonNum(episode.number)}`;
   title.className = "title";
+  title.style.textAlign = "center";
+  if (episode.name == "" || episode.name == null) {
+    title.innerText = "Tv show data is currently unavailable";
+  } else {
+      title.innerHTML = `${episode.name}: S${formatSeasonNum(episode.season)}E${formatSeasonNum(episode.number)}`;
+  }
+  
 
   let img = document.createElement("img");
   img.style.margin = "0 auto";
   img.style.width = "99%";
   // img.style.paddingLeft = "2em";
   // img.style.paddingRight = "2em";
-  img.src = episode.image.medium;
+  if (episode.image == null || episode.image == "") {
+    //console.log("test",episode.image)
+    img.src = "./img/error.jpg" 
+  } else {
+   img.src = episode.image.medium;
+  }
 
+  
   let summary = document.createElement("p");
   summary.style.texAlign = "center";
-  summary.innerHTML = `${episode.summary}`;
   summary.className = "summary";
+  if (episode.summary == ""|| episode.summary == null) {
+  summary.innerText = "Information for this episode is currently unavailable"  
+  } else {
+  summary.innerHTML = `${episode.summary}`;   
+  }
+
   episodeContainer.appendChild(title);
   episodeContainer.appendChild(img);
   episodeContainer.appendChild(summary);
@@ -141,19 +166,40 @@ function addSingleEpisode(episode) {
 
 // container for the single episodes
 const addAllEpisodes = (episodeList) => {
+  // addDropDownMenu(episodeList);
+  upDateDropDownMenu(episodeList);
   const rootElem = document.getElementById("root");
-  let styleContainer = document.createElement("div");
-  styleContainer.setAttribute("id", "styleContainer");
-  styleContainer.className = "style-container";
-  styleContainer.style.width = "98%";
-  styleContainer.style.flexWrap = "wrap";
-  styleContainer.style.margin = "0 auto";
+  let styleContainer = document.getElementById("styleContainer");
 
+  if (styleContainer === null) {
+    styleContainer = document.createElement("div");
+    styleContainer.setAttribute("id", "styleContainer");
+    styleContainer.className = "style-container";
+    styleContainer.style.width = "98%";
+    styleContainer.style.flexWrap = "wrap";
+    styleContainer.style.margin = "0 auto";
+    rootElem.appendChild(styleContainer);
+  }
   episodeList.forEach((episode) => {
     styleContainer.appendChild(addSingleEpisode(episode));
   });
-  rootElem.appendChild(styleContainer);
 };
+
+function upDateDropDownMenu(episodeList) {
+  //goes through the options removes the options and recreates the opitons in the second loop
+  let select = document.getElementById("select");
+  if (select !== null) {
+    for (let index = select.options.length; index > 0; index--) {
+      select.remove(index);
+    }
+
+    episodeList.forEach((episode) => {
+      let option = document.createElement("option");
+      option.innerHTML = `${episode.name}: S${formatSeasonNum(episode.season)}E${formatSeasonNum(episode.number)}`;
+      select.appendChild(option);
+    });
+  }
+}
 
 const addDropDownMenu = (episodeList) => {
   let option;
@@ -168,12 +214,11 @@ const addDropDownMenu = (episodeList) => {
   } else {
     // let options = select.getElementsByTagName("option");
     // console.log("test", options);
-    
-    for (let index = select.options.length; index > 0; index--) { // this will go through the loop starting at the end and for each element it will remove all the options before it rebuilds it 
-    select.remove(index);
-    }
-
     // select.innerHTML = "";
+    for (let index = select.options.length; index > 0; index--) {
+      // this will go through the loop starting at the end and for each element it will remove all the options before it rebuilds it
+      select.remove(index);
+    }
   }
 
   episodeList.forEach((episode) => {
@@ -230,14 +275,12 @@ const addCounter = (_) => {
 function updateCounter(numberOfEpisodes, totalEpisodes) {
   // counter for number of episode found in search
   let counterDisplay = document.querySelector("#counterDisplayID");
-
   counterDisplay.innerText = `Displaying ${numberOfEpisodes} / ${totalEpisodes} episodes`; // update of the counter
 }
 
 const addSearchBox = (episodeList) => {
-  let episodeContainers = document.getElementsByClassName(
-    "episode-container-class-name"
-  ); // An array of episode container
+  let episodeContainers = document.getElementsByClassName("episode-container-class-name"); // An array of episode container
+  // console.log(episodeContainers);
 
   let inputBox = document.getElementById("inputBoxId");
   if (inputBox === null) {
@@ -254,20 +297,45 @@ const addSearchBox = (episodeList) => {
     const searchString = event.target.value.toLowerCase(); // gets search string from input
     let episodeIndex = 0;
     let episodeCount = 0; // this is the number of episodes that match the key word search string
+    // console.log(episode);
+    let informationMissing = document.createElement("p");
+    let errorContainer = document.getElementById("styleContainer")
+    
+    // access the data through the array
 
-    episodeList.forEach((episode) => {
-      if (
-        episode.name.toLowerCase().includes(searchString) ||
-        episode.summary.toLowerCase().includes(searchString)
-      ) {
+
+    episodes.forEach((episode) => {
+      // if ((episode.name == null || episode.name == "") || (episode.summary == null || episode.summary == "")){
+      //   // errorContainer.innerText = "Sorry, the keyword does not exist in database";
+      //   // console.log(errorContainer.innerText)
+      //   // informationMissing.innerText = "Sorry, the keyword does not exist in database"
+      //   // errorContainer.appendChild(informationMissing)
+      //   //console.log(informationMissing.innerText)
+      // }
+      if (episode.name.toLowerCase().includes(searchString) || episode.summary.toLowerCase().includes(searchString))
+      {
         episodeCount++;
         showEpisode(episodeContainers[episodeIndex]);
-      } else {
+       }
+       else if((episode.name == null || episode.name == "") || (episode.summary == null || episode.summary == "")){
         hideEpisode(episodeContainers[episodeIndex]);
+        informationMissing.innerText = "Sorry, the keyword does not exist in database"
+        errorContainer.appendChild(informationMissing)
+        //console.log(informationMissing.innerText)
+      }else {
+         hideEpisode(episodeContainers[episodeIndex]);
+         informationMissing.innerText = "Sorry, the keyword does not exist in database";
+         errorContainer.appendChild(informationMissing);
+         //console.log(errorContainer)
       }
       episodeIndex++;
     });
     updateCounter(episodeCount, episodeList.length);
+     
+
+
+
+
   });
 
   return inputBox;
@@ -275,26 +343,35 @@ const addSearchBox = (episodeList) => {
 
 const addHeader = (episodeList, showData) => {
   const rootElem = document.getElementById("root");
-  let navBarContainer = document.createElement("div");
   let body = document.querySelector("body");
 
-  navBarContainer.style.display = "flex";
-  navBarContainer.setAttribute("id", "navBarContainer");
-  body.insertBefore(navBarContainer, rootElem);
+  let navBarContainer = document.getElementById("navBarContainer");
 
-  const showSelector = addShowSelector(showData);
-  navBarContainer.appendChild(showSelector);
+  if (navBarContainer === null) {
+    //
+    navBarContainer = document.createElement("div");
+    navBarContainer.style.display = "flex";
+    navBarContainer.setAttribute("id", "navBarContainer");
 
-  const inputBox = addSearchBox(episodeList);
-  navBarContainer.appendChild(inputBox);
-  const counter = addCounter();
-  navBarContainer.appendChild(counter);
-  updateCounter(episodeList.length, episodeList.length);
+    body.insertBefore(navBarContainer, rootElem);
+
+    const showSelector = addShowSelector(showData);
+    navBarContainer.appendChild(showSelector);
+
+    const inputBox = addSearchBox(episodeList);
+    navBarContainer.appendChild(inputBox);
+    const counter = addCounter();
+
+    navBarContainer.appendChild(counter);
+    updateCounter(episodeList.length, episodeList.length);
+  }
+
   const dropDown = addDropDownMenu(episodeList);
   navBarContainer.appendChild(dropDown);
 };
 
 function makePageForEpisodes(episodeList) {
+  episodes = episodeList;
   const tvShowData = getAllShows();
   addAllEpisodes(episodeList);
   addHeader(episodeList, tvShowData);
@@ -303,15 +380,28 @@ function makePageForEpisodes(episodeList) {
 
 function setup() {
   // fetches the data from the tv show website
-  getAllEpisodesApi(82);
+  const shows = getAllShows().sort(compare);
+  // console.log(shows[0]);
+  const showId = shows[0].id;
+  fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
+    .then((response) => response.json().then((data) => data)) // remove the end of the json once bug is fixed with the header// return response.json this gathers the data
+    .then((allEpisodes) => makePageForEpisodes(allEpisodes)) // this does nt get called until we get the data
+    .catch((error) => console.log(error));
+  
+  //getAllEpisodesApi(82);
 }
+
+let episodes; // global variable that contains the episodes/ data from the api
 
 function getAllEpisodesApi(id) {
   // function to get the apis
   let arrayOfEpisodes;
   fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
-    .then((response) => response.json().then((data) => data))
-    .then((allEpisodes) => makePageForEpisodes(allEpisodes))
+    .then((response) => response.json().then((data) => data)) // return response.json this gathers the data
+    .then((allEpisodes) => {
+      episodes = allEpisodes;
+      addAllEpisodes(allEpisodes);
+    }) // this does not get called until we get the data
     .catch((error) => console.log(error));
 }
 window.onload = setup; // when the browser is load call set up
